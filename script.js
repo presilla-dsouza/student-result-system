8cd51e9d58fd9e4afb1c259fb43fd7f76d1aa559
presilla-dsouza <presillad1@gmail.com>
2024-01-15 23:07:14 +0530

    newone

---
+++ script.js
@@ -0,0 +1,23 @@
+function validateLogin() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let errorMessage = document.getElementById("errorMessage");
+    
+  
+    if (username !== "admin") {
+      errorMessage.innerText = "Invalid username. Please try again.";
+    } else if (password !== "admin@123") {
+      errorMessage.innerText = "Invalid password. Please try again.";
+    } else {
+  
+      localStorage.setItem('isLoggedIn','true')
+      window.location = './dashboard.html'
+    }
+  }
+  
+  
+  
+  function logOut() {
+      localStorage.removeItem("isLoggedIn");
+      window.location = "./AdminPage.html";
+    }
